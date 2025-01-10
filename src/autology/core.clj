bf8236101bd4e446c6f20991e0c0ba928e70bfa3
Dave Kimber <davekimber@riverford.co.uk>
2025-01-10 12:03:39 +0000

    Add interpreter editing util functions

--- src/autology/core.clj
+++ src/autology/core.clj
@@ -2,44 +2,9 @@
   (:gen-class)
   (:require [clojure.walk :refer [postwalk]]))
 
-;; If we 'mark' our forms in the interpreter with qualified keywords we can use these as references for modifying it.
-(def example-i '(:atl/outer
-                (fn
-                  [e env]
-                  (:atl/coll-switch
-                   (if-not
-                       (coll? e)
-                     (:atl/scalar-branch
-                      (if
-                          (symbol? e)
-                        (:atl/symbol-branch (get env e))
-                        (:atl/self-evaluating e)))
-                     ;; @TODO: do the whole initial-interpreter
-                     (:atl/coll-branch (other e)))))))
-
-;; We can supply a new value for the content of any marker
-(defn replace-marker
-  [interpreter marker new-marker-form]
-  (postwalk
-   (fn [expr]
-     (if (and (list? expr)
-              (= marker (first expr)))
-       (list marker new-marker-form)
-       expr))
-   interpreter))
-
-;; We can remove all the markers before evaluating the interpreter
-(defn strip-markers [e]
-  (if (list? e)
-    (if (and (keyword? (first e))
-             (= "atl" (namespace (first e))))
-      (first (map strip-markers (rest e)))
-      (map strip-markers e))
-    e))
-
-;; @TODO: try this for real, clean up a bit, get them in the initial environment so we can use them from within autology
 
 
+;; @TODO: we want some easy `with-*i*` function/special form to allow us to switch between existing interpreters.
 
 
 
@@ -48,41 +13,63 @@
 ;; `autology.core/evaluate` defined below which will get the
 ;; interpreter from the execution environment.
 ;;
-;; We bind `*ns*` to be `autology.core` so we ensure we're able to
-;; access it regardless of how autology is run (REPL, script, jar
-;; etc).
+;; We mark each section with an `:atl/foo` qualified keyword, this
+;; allows us to easily replace sections in our program (including
+;; inserting new markers for subsequent modification).
 ;;
 ;; We want to keep this interpreter fully defined in this one
 ;; expression, no named functions, only lambdas, that way it's easy to
 ;; modify from within an autology program.
 (def initial-interpreter
-  '(fn [e env]
-     (if-not (coll? e)
-       ;; evaluate an atom
-       (if (symbol? e)
-         (get env e)
-         e)
-       ;; evaluate a list
-       (case (first e)
-         ;; we need our own symbol for quote so the code we're writing isn't seen as the clojure quote special form.
-         qu (second e)
+  '(:atl/all
+    (fn [e env]
+      (:atl/list-switch
+       (if-not (list? e)
+         ;; evaluate an atom
+         (:atl/eval-atom
+          (if (symbol? e)
+            (get env e)
+            (:atl/self-evaluating e)))
+         ;; evaluate a list
+         (:atl/eval-list
+          (case (first e)
+            ;; we need our own symbol for quote so the code we're
+            ;; writing isn't seen as the clojure quote special form.
+            qu (:atl/quote (second e))
 
-         ;; @TODO: need the func special form so we can start saving
-         ;; our interpreter modification functions into the
-         ;; environment
+            ;; @TODO: need the func special form so we can start saving
+            ;; our interpreter modification functions into the
+            ;; environment
 
-         bind (let [bindings (partition 2 (second e))]
-                (evaluate
-                 (nth e 2)
-                 (reduce (fn [acc-env [n v]]
-                           (assoc acc-env n (evaluate v acc-env)))
-                         env
-                         bindings)))
+            bind (:atl/bind
+                  (let [bindings (partition 2 (second e))]
+                    (evaluate
+                     (nth e 2)
+                     (reduce (fn [acc-env [n v]]
+                               (assoc acc-env n (evaluate v acc-env)))
+                             env
+                             bindings))))
+            
+            ;; default to function application
+            (:atl/function-application
+             (apply (evaluate (first e))
+                    (map (fn [arg] (evaluate arg env))
+                         (rest e)))))))))))
 
-         ;; default to function application
-         (apply (evaluate (first e))
-                (map (fn [arg] (evaluate arg env))
-                     (rest e)))))))
+(defn replace-marker
+  "Replace the contents of a marked section of an interpreter with a new
+  expression.
+
+  ;; @TODO: do we want this behaviour? we might be drasticallly changing the expression, the orginal marker might not make sense? I reckon change it, the user can add in the new marker, or we could have a flag/opt-arg?
+  The new section will be wrapped in the existing marker."
+  [interpreter marker new-marker-form]
+  (postwalk
+   (fn [expr]
+     (if (and (list? expr)
+              (= marker (first expr)))
+       (list marker new-marker-form)
+       expr))
+   interpreter))
 
 (def initial-env
   {'+ +
@@ -96,17 +83,33 @@
    'list list
    'concat concat
 
+   ;; Interpreter-editing utils
+   'replace-marker replace-marker
+   ;; @TODO: add get-marker? so we can bind it and work on it more easily?
+
    ;; the autology interpreter
    '*i*
    initial-interpreter})
 
+;; We can remove all the markers before evaluating the interpreter
+(defn strip-markers
+  "Remove all the `:atl/foo` markers from a interpreter data structure."
+  [e]
+  (if (list? e)
+    (if (and (keyword? (first e))
+             (= "atl" (namespace (first e))))
+      (first (map strip-markers (rest e)))
+      (map strip-markers e))
+    e))
+
 (defn evaluate
+  "Grab the interpreter out of the execution environment, strip all
+  the markers out, evaluate it (as a Clojure function) and use it to
+  evaluate the expression."
   ([e]
    (evaluate e initial-env))
   ([e env]
-   ;; grab the interpreter out of the execution environment ad use it
-   ;; to evaluate the expression.
-   ((eval (get env '*i*)) e env)))
+   ((eval (strip-markers (get env '*i*))) e env)))
 
 (def eval-string (comp evaluate read-string))
 
@@ -124,10 +127,12 @@
 
 (defn -main
   [& args]
+  ;; We bind `*ns*` to be `autology.core` so we ensure we're able to
+  ;; access it regardless of how autology is run (REPL, script, jar
+  ;; etc).
   (binding [*ns* (the-ns 'autology.core)]
     (if args
       (prn (eval-file (first args)))
-
       (do
         (print "repl:\n")
         (flush)
